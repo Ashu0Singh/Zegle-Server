18c539b70f0d2a12359e7974a3ba3da1f982bf8a
Ashutosh Singh <ashusingh1417@gmail.com>
2024-12-24 02:24:32 +0530

    feat: updated route

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -55,7 +55,7 @@ export const SaveUser = async (req, res) => {
 export const VerifyUserEmail = async (req, res) => {
     const { email, token } = req.body;
     const hash = await bcrypt.hash(`${email}-${EMAIL_SALT}`, 8);
-    const link = `${CLIENT_URL}/verify?token=${hash}`;
+    const link = `${CLIENT_URL}/user/verify?token=${hash}`;
 
     if (token) {
         if (!email) {
@@ -67,9 +67,9 @@ export const VerifyUserEmail = async (req, res) => {
             );
             if (compare) {
                 await updateUserDataByEmail(email, { verified: true });
-                return res.status(200).json({ message: "Token verified" });
+                return res.status(200).json({ message: "User Verified" });
             } else {
-                return res.status(400).json({ error: "Invalid token" });
+                return res.status(400).json({ error: "Invalid User or Token" });
             }
         }
     }
